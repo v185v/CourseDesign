e75b22f562da3f784cac5e0501fa670090965b49
v185v <chen13506093007@163.com>
2026-05-15 20:36:20 +0800

    chore:添加类注释

--- Goods.cpp
+++ Goods.cpp
@@ -4,11 +4,25 @@
 
 Goods::Goods() : price(0), quantity(0) {}
 
+/// <summary>
+/// 货物编号、货物名称、生产厂家、生产日期、货物单价、货物数量，入库时间
+/// </summary>
+/// <param name="id"></param>
+/// <param name="name"></param>
+/// <param name="mfg"></param>
+/// <param name="pDate"></param>
+/// <param name="price"></param>
+/// <param name="qty"></param>
+/// <param name="sDate"></param>
 Goods::Goods(std::string id, std::string name, std::string mfg, 
              Date pDate, double price, int qty, Date sDate)
     : id(id), name(name), manufacturer(mfg), productionDate(pDate), 
       price(price), quantity(qty), storageDate(sDate) {}
 
+/// <summary>
+/// 将 Goods 对象序列化为以逗号分隔的 CSV 字符串（字段顺序：id、name、manufacturer、productionDate、price、quantity、storageDate）。
+/// </summary>
+/// <returns>包含对象各字段的 std::string，字段按顺序以逗号分隔；日期通过 productionDate.toString()/storageDate.toString() 转为字符串，数值通过 std::to_string 转为字符串。</returns>
 std::string Goods::toCSV() const {
     return id + "," + name + "," + manufacturer + "," + 
            productionDate.toString() + "," + std::to_string(price) + "," + 
