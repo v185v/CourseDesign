421fb4d5cc8ea4cfdf61fd9c74244f1900472433
v185v <chen13506093007@163.com>
2026-05-17 01:27:53 +0800

    fix:修复终端中文乱码 chore:添加gitignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "WarehouseManager.h"
+#include <cstdlib>
 
 int main() {
+    // 强行将 Windows 控制台的编码切换为 UTF-8 (代号 65001)
+    system("chcp 65001");
+    
     // 实例化系统管理者
     WarehouseManager app;
 
